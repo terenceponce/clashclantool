7419e8e50d81d7ffdeda9380ae72d0ad7bbe8656
Terence Ponce <terenceponce@gmail.com>
2015-02-19 20:15:32 +0800

    Add clan page and add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219003346) do
+ActiveRecord::Schema.define(version: 20150219072019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150219003346) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "clan_id"
+    t.string   "name",                   default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
