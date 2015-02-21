05399c368f0e8d78cc40a7fb00a73b946da6256b
Terence Ponce <terenceponce@gmail.com>
2015-02-21 11:52:40 +0800

    Make war position start at 1

--- app/actions/creates_war_composition_action.rb
+++ app/actions/creates_war_composition_action.rb
@@ -7,7 +7,7 @@ class CreatesWarCompositionAction
 
     Opponent.transaction do
       context.war.size.size.times.each do |position|
-        context.war.opponent_clan.members.create(war_position: position)
+        context.war.opponent_clan.members.create(war_position: position + 1)
       end
     end
   end
