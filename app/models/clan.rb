d429c6267af9f8b3d72b868488b769e116d2d9f8
Terence Ponce <terenceponce@gmail.com>
2015-02-25 21:57:00 +0800

    Associate War with Clan and OpponentClan because I forgot to do it earlier

--- app/models/clan.rb
+++ app/models/clan.rb
@@ -1,3 +1,4 @@
 class Clan < ActiveRecord::Base
   has_many :members, class_name: 'User'
+  has_many :wars
 end
--- app/models/opponent_clan.rb
+++ app/models/opponent_clan.rb
@@ -1,3 +1,4 @@
 class OpponentClan < ActiveRecord::Base
   has_many :members, class_name: 'Opponent'
+  has_many :wars
 end
